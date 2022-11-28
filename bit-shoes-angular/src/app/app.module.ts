3830cbc17eafb5c14e0d088bd8e59d37ba05cda0
cristtian177 <cristtian177@gmail.com>
2022-11-28 18:55:16 -0500

    firts commit

---
+++ bit-shoes-angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/carousel-item/carousel.component';
+import { HomeComponent } from './routes/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { OutletComponent } from './components/outlet/outlet.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarouselComponent,
+    HomeComponent,
+    NavbarComponent,
+    BannerComponent,
+    OutletComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
